51fcc91fad05b9f14a4a33e1723dd4e9bff11f29
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-03-02 17:43:43 -0500

    Actualizar

--- Myapp/Alpha_web/tests.py
+++ Myapp/Alpha_web/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_home_page(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome to the home page")
