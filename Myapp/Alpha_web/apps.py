b97f32e1198e0af6d15ad06c2935674f72a55d6b
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-03-05 11:23:38 -0500

    ACTUALIZAR

--- Myapp/Alpha_web/apps.py
+++ Myapp/Alpha_web/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AlphaWebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Alpha_web'
+    verbose_name = 'Alpha Web Application'
--- Myapp/Alpha_web/models.py
+++ Myapp/Alpha_web/models.py
@@ -17,3 +17,21 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order of {self.quantity} {self.product.name}(s) on {self.order_date}"
+        class Customer(models.Model):
+            first_name = models.CharField(max_length=50)
+            last_name = models.CharField(max_length=50)
+            email = models.EmailField(unique=True)
+            phone = models.CharField(max_length=20, blank=True, null=True)
+            address = models.TextField(blank=True, null=True)
+
+            def __str__(self):
+                return f"{self.first_name} {self.last_name}"
+
+        class Order(models.Model):
+            product = models.ForeignKey(Product, on_delete=models.CASCADE)
+            customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+            quantity = models.IntegerField()
+            order_date = models.DateTimeField(auto_now_add=True)
+
+            def __str__(self):
+                return f"Order of {self.quantity} {self.product.name}(s) by {self.customer.first_name} on {self.order_date}"
