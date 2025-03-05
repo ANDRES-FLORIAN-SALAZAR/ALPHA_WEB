cf7650ffa57c40fc4e74221219da8b5993928755
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-03-05 11:11:31 -0500

    ACTUALIZAR

--- Myapp/Alpha_web/models.py
+++ Myapp/Alpha_web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order of {self.quantity} {self.product.name}(s) on {self.order_date}"
