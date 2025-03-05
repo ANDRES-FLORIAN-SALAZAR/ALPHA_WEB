86fa395f022d35d1857f48b02e39e1b1d04d5fe9
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-03-04 20:20:08 -0500

    ACTUALIZAR

--- Myapp/Alpha_web/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlphaWebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Alpha_web'
-    verbose_name = 'Alpha Web Application'
