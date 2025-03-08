81effde6ebddad9c576ec590a2d5a25f8c7f22e0
ANDRES FLORIAN <DUVANFLORIANSALAZAR@GMAIL.COM>
2025-03-07 23:47:04 -0500

    ACTUALIZAR

--- Alpha_web/Aplicacion/urls.py
+++ Alpha_web/Aplicacion/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from Aplicacion import views
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home, name='Home'),
-]
-
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home, name='home'),
+    path('Inicio/', views.Inicio, name='Inicio'),
+    path('Planes/', views.Planes, name='Planes'),
+    path('Inicio_Sesión/', views.Inicio_Sesión, name='Inicio_Sesión'),
+    path('Registro/', views.Registro, name='Registro'),
+    path('Contraseñas/', views.Contraseñas, name='Contraseñas'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Alpha_web/Aplicacion/views.py
+++ Alpha_web/Aplicacion/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
+def Inicio(request):
+    return render(request, 'Inicio.html')   
+
+def Planes(request):
+    return render(request, 'PLanes.html')
+
+def Registro(request):
+    return render(request, 'Registro.html')
+
+def Inicio_Sesión(request):
+    return render(request, 'Inicio_Sesión.html')
+
+def Contraseñas(request):
+    return render(request, 'Contraseñas.html')
